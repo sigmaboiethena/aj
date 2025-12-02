26aca5970ce2447779363d5eb8dcbd6a13c52f31
sigmaboiethena <celestialbozos@gmail.com>
2025-12-02 14:57:14 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -251,27 +251,28 @@ local function stealChecker(name, money, owner)
     observePodium(tPodium, {name = name, money = money, owner = owner})
 end
 
--- Players.PlayerAdded:Connect(function(player)
---     if player ~= LOCAL_PLAYER then
---         return
---     end
-
---     if not isfile('stealnotify.json') then 
---         return 
---     end
-
---     local content = readfile('stealnotify.json')
---     local success, data = pcall(function() return HttpService:JSONDecode(content) end)
---     if not success or type(data) ~= "table" then
---         warn("[STEAL] ⚠️ Error reading stealnotify.json")
---         return
---     end
-
---     if game.JobId ~= data.jobid then
---         return
---     end
---     stealChecker(data.name, data.money, data.owner)
--- end)
+Players.PlayerAdded:Connect(function(player)
+    print('tu')
+    if player ~= LOCAL_PLAYER then
+        return
+    end
+    print('dasads')
+    if not isfile('stealnotify.json') then 
+        return 
+    end
+    print('sahur')
+    local content = readfile('stealnotify.json')
+    local success, data = pcall(function() return HttpService:JSONDecode(content) end)
+    if not success or type(data) ~= "table" then
+        warn("[STEAL] ⚠️ Error reading stealnotify.json")
+        return
+    end
+    print('ketupat')
+    if game.JobId ~= data.jobid then
+        return
+    end
+    stealChecker(data.name, data.money, data.owner)
+end)
 
 
 -- =========================================================
